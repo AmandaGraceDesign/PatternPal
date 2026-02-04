b89f17176f8ac483c6bb3c5eee1b08b79a712c4b
Mandy <contact@amandagracedesign.com>
2026-02-03 19:10:11 -0500

    Remove middleware.ts to fix Vercel build

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/api/stripe/webhook"],
-});
-
-export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
-};
